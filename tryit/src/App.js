af50fecf0bd14f0afdfb502dcfd81e4e5668e1da
Aiko <jokoaiko8b@gmail.com>
2024-08-25 20:00:11 +0700

    anjay

--- tryit/src/App.js
+++ tryit/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <dl class="divide-y divide-gray-100">
             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
               <dt class="text-sm font-medium leading-6 text-gray-900">Full name</dt>
-              <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Gibran Rakabuming Raka</dd>
+              <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Bernadya</dd>
             </div>
             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
               <dt class="text-sm font-medium leading-6 text-gray-900">Application for</dt>
