fc79cfcd042c2455e83ea54ce777566d6d34963d
Aiko <jokoaiko8b@gmail.com>
2024-08-25 19:55:34 +0700

    mencoba electron

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+const url = require('url')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    autoHideMenuBar: true,
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.webContents.openDevTools();
+
+  
+
+  win.loadFile(path.join(__dirname, 'tryit/build/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
