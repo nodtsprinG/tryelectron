ea87b940506db08398e38758c07b545156532897
Joko Aiko <155278705+nodtsprinG@users.noreply.github.com>
2024-08-25 20:01:23 +0700

    Update email

--- tryit/src/App.js
+++ tryit/src/App.js
@@ -21,7 +21,7 @@ function App() {
             </div>
             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
               <dt class="text-sm font-medium leading-6 text-gray-900">Email address</dt>
-              <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">jokoaikopersonaluse@gmail.com</dd>
+              <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">bernadyapersonaluse@gmail.com</dd>
             </div>
             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
               <dt class="text-sm font-medium leading-6 text-gray-900">Salary expectation</dt>
